ba346308a66e9ee7dba13808789cd9487f59b2f3
Kun Béla Roland <43463212+KBRoli@users.noreply.github.com>
2018-09-21 21:19:05 +0300

    Update drill3.cpp

--- drill3.cpp
+++ drill3.cpp
@@ -19,7 +19,7 @@ int main()
 	cout<<"Im writting this letter 'cause we speak a long time since the last time and I want to know whats up with you nowdays?\n";
 	cout<<"Have you seen "<< friend_name<< "since the last time?";
 	if(friend_sex== 'f') cout<<"If you see "<<friend_name<<"ask her please to organise another meet.\n";
-	else(friend_sex== 'm') cout<<"If you see "<<friend_name<<"ask him please to organise another meet.\n";
+	else if (friend_sex== 'm') cout<<"If you see "<<friend_name<<"ask him please to organise another meet.\n";
 	if(age <=0 || age >=101) simple_error("You are just kidding!");
 	else  cout<<"Yeah, i heard about that you are at age "<<age<<"\n";
 	if (age<16) cout<<"Next year you will be " <<age+1<<"\n";
